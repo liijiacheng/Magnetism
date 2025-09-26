d8dfc18feb4f5cf691d8f6c7b7ec36cbcaf5743d
lijiacheng <lijiacheng657@gmail.com>
2025-09-26 11:52:31 +0800

    add anisotropicTMM.py, unfinished.

---
+++ anisotropicTMM.py
@@ -0,0 +1,80 @@
+import numpy as np
+from scipy import constants
+import matplotlib.pyplot as plt
+
+class anisotropicTMM:
+    '''
+    Anisotropic Transfer Matrix Method.
+    Parameters:
+        epsilon: array
+            complex dielectric constant tensor, dim=(num_layers, 3, 3, len(wavelength)).
+        d: list [nm]
+            thickness of each layer, assumed to be surrounded by air.
+        wavelength: array [nm]
+            wavelength of light.
+        phi: float [rad]
+            incident angle, assumed to be in x-z plane.
+    '''
+    def __init__(self, epsilon, d, wavelength, phi):
+        self.epsilon = epsilon
+        self.d = d * 1e-9 # nm to m
+        self.wavelength = wavelength * 1e-9 # nm to m
+        self.phi = phi
+        self.num_layers = len(d)
+        self.ratio_0 = np.sqrt(constants.epsilon_0 / constants.mu_0)
+        self.alpha = np.sin(phi)
+        self.beta = 0 # incident plane is x-z plane
+        self.k0 = 2 * np.pi / self.wavelength # wavevector in vacuum
+
+    def propagate_matrix(self):
+        '''
+        Calculate the propagate matrixs for each layer.
+        Returns:
+            S: array
+                propagate matrix, dim=(num_layers, 4, 4, len(wavelength)).
+        '''
+        S = np.zeros((self.num_layers, 4, 4, len(self.wavelength)), dtype=complex)
+        for i in range(self.num_layers):
+            eps = self.epsilon[i]
+            a = self.alpha
+            b = self.beta
+            S[i,:,:,:] = np.array([
+                [-a*eps[2,0]/eps[2,2], -a*eps[2,1]/eps[2,2], a*b/eps[2,2]/self.ratio_0, (1-a**2/eps[2,2])/self.ratio_0],
+                [-b*eps[2,0]/eps[2,2], -b*eps[2,1]/eps[2,2], -(1-b**2/eps[2,2])/self.ratio_0, -a*b/eps[2,2]/self.ratio_0],
+                [(-a*b-eps[1,0]+eps[2,0]*eps[1,2]/eps[2,2])*self.ratio_0, (a**2-eps[1,1]+eps[2,1]*eps[1,2]/eps[2,2])*self.ratio_0, -b*eps[1,2]/eps[2,2], a*eps[1,2]/eps[2,2]],
+                [(-b**2+eps[0,0]-eps[2,0]*eps[0,2]/eps[2,2])*self.ratio_0, (a*b+eps[0,1]-eps[2,1]*eps[0,2]/eps[2,2])*self.ratio_0, b*eps[0,2]/eps[2,2], -a*eps[0,2]/eps[2,2]]
+            ]).reshape(4,4,len(self.wavelength))
+        return S
+    
+    def transfer_matrix(self):
+        '''
+        Calculate the transfer matrixs for each layer.
+        Returns:
+            M: array
+                transfer matrix, dim=(4, 4, len(wavelength)).
+        '''
+        kS = self.k0 * self.propagate_matrix()
+        M = np.zeros((4, 4, len(self.wavelength)), dtype=complex)
+        for l in range(len(self.wavelength)):
+            M[:,:,l] = np.eye(4, dtype=complex)
+            for i in range(self.num_layers):
+                d = self.d[i]
+                kS_il = kS[i,:,:,l]
+                eigvals, eigvecs = np.linalg.eig(kS_il)
+                M_il = np.diag(np.exp(1j * eigvals * d))
+                M_il = eigvecs @ M_il @ np.linalg.inv(eigvecs)
+                M[:,:,l] = M_il @ M[:,:,l]
+        # kSd=np.einsum('ijkl,i->ijkl', kS_i, d)
+        # M_lis=np.exp(1j * kSd)
+        # M = M_lis[0]
+        # for i in range(1,self.num_layers):
+        #     M = np.einsum('jkl,kml->jml', M_lis[i], M)
+        return M
+    
+    def reflectance(self):
+        '''
+        Calculate the reflectance.
+        Returns:
+            R: array
+                reflectance, dim=(len(wavelength),).
+        '''
--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 print('this is initial commit')
+
+import numpy as np
+
+S=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(S.reshape(3,2,2))
+print(S.reshape(2,3,2))
